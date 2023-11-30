51f50f12836dc147d328962347fabc91e2b63d28
robin nganga smith <saintofcookies.2021@gmail.com>
2023-11-30 11:07:59 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+def countdown(count = 10):
+  while count > 0:
+    print(f"{count} SECOND(S)!")
+    count -= 1
+    print("HAPPY NEW YEAR!")
+
+
+
+   def countdown_with_sleep(count = 10):
+     while count > 0:
+      print(f"{count} SECOND(S)!")
+      time.sleep(1)
+      count -= 1
+      print("HAPPY NEW YEAR!")
